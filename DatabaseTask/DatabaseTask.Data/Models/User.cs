2206d97026c9a822924e16744b52f5ccf30c8a37
Kundratsk <kundratsk@gmail.com>
2026-06-13 16:28:29 +0300

    Update DatabaseTask

---
+++ DatabaseTask/DatabaseTask.Data/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseTask.Data.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Address { get; set; }
+}
